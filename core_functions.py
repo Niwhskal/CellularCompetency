4c030f582648e0e28388b454e87263fb8c621d1a
Your Name <lakshwin045@gmail.com>
2021-11-16 19:59:46 +0000

    unified

---
+++ core_functions.py
@@ -0,0 +1,109 @@
+import math
+import numpy as np
+
+
+class HelperFuncs():
+  def __init__(self, conf):
+    self.config = conf
+    self.ncr = math.factorial(self.config['SIZE']) / (math.factorial(self.config['SIZE']-2)* 2)
+
+  def fitness(self, o):
+    inv_count = 0
+    for i in range(len(o)):
+      for j in range(i + 1, len(o)):
+        if (o[i] < o[j]):
+          inv_count += 1
+    return inv_count/self.ncr
+
+
+  def selection(self, ory, fi):
+    organisms = ory.copy()
+    fitne = fi.copy()
+    fit_index = {k: i for k, i in enumerate(fitne)}
+    fitness_organisms = {k: v for k, v in sorted(fit_index.items(), key=lambda item: item[1], reverse=True)} 
+    orgs_keys = [k for k,v in fitness_organisms.items()] 
+    orgs_vals = list(fitness_organisms.values())
+
+    new_orgs_keys = orgs_keys[: round(0.1*self.config['N_organisms'])]
+    new_orgs_vals = orgs_vals[: round(0.1*self.config['N_organisms'])]
+
+    new_orgs = [organisms[j] for j in new_orgs_keys]
+    max_fitness = new_orgs_vals[0] 
+    return new_orgs, max_fitness 
+
+
+  def shuffle_mutation(self, organism):
+    organisms = organism.copy()
+    L = len(organisms)
+    while (len(organisms)<= self.config['N_organisms']):
+      new_org = organisms[np.random.randint(L)].copy() 
+      np.random.shuffle(new_org)
+      organisms = np.append(organisms, [new_org], axis =0) 
+
+    return organisms
+
+
+  def mutation_flip(self, organi):
+    organisms = organi.copy()
+
+    for i in range(len(organisms)):
+      if np.random.rand(1) > self.config['Mutation_Probability']:
+        organisms[i][np.random.randint(self.config['SIZE'])] = np.random.randint(low = self.config['LOW'], high=self.config['HIGH'])
+
+    return organisms     
+
+
+  def crossover_mutation(self, organis):
+    organisms = organis.copy()
+    L = len(organisms)
+    while (len(organisms) < self.config['N_organisms']):
+      i = np.random.randint(L)
+      j = np.random.randint(L)
+      if i!=j:
+        random_pair = (i, j)
+      else:
+        continue
+
+      cross_over_point = np.random.randint(low = self.config['LOW']+10, high=self.config['HIGH'])
+      new_org_1 = organisms[random_pair[0]]
+      new_org_2 = organisms[random_pair[1]]
+
+  #    print(random_pair, cross_over_point, new_org_1, new_org_2)
+
+      temp = new_org_1[:cross_over_point].copy()
+      new_org_1[:cross_over_point] = new_org_2[:cross_over_point]
+      new_org_2[:cross_over_point] = temp 
+
+  #    print(temp, new_org_1, new_org_2)
+  #    print('\n')
+      organisms = np.append(organisms, [new_org_1, new_org_2], axis =0) 
+  #    print(organisms)
+  #    print('\n')
+  #    print('\n ====')
+    return organisms
+
+
+  def reorganize(self, orgas):
+    orgs = orgas.copy()
+    count = 0
+    for nos, org in enumerate(orgs):
+      n = 0
+      while n < len(org)-1:
+        if org[n] > org[n+1]:
+          temp = org[n]
+          org[n] = org[n+1] 
+          org[n+1] = temp
+          count +=1
+        n+=1
+    return orgs, count/len(orgs)
+
+
+  if __name__=='__main__':
+    conf = {
+      'LOW' :1,
+      'HIGH':101,
+      'N_organisms' : 100,
+      'SIZE' : 100,
+      'Mutation_Probability' : 0.6,
+      }
+    funcs = HelperFuncs(conf)
---
+++ main.py
@@ -0,0 +1,106 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from core_functions import HelperFuncs
+from sklearn.manifold import TSNE
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+
+class TwoFit():
+  def __init__(self, conf, cfs):
+
+    self.config = conf
+    self.cfs = cfs
+    self.init_organisms = np.random.randint(low=self.config['LOW'], high=self.config['HIGH'], size=(self.config['N_organisms'], self.config['SIZE']))
+
+  def run_ga(self):
+
+    generation = 0
+
+    self.raw_fitness = []
+    self.app_fitness = []
+
+    self.tsne_fitness=[]
+
+    self.competent_mod, _ = self.cfs.reorganize(self.init_organisms)
+
+    r_fit = [self.cfs.fitness(i) for i in self.init_organisms]
+    ap_fit = [self.cfs.fitness(j) for j in self.competent_mod]
+
+    max_raw = max(r_fit)
+
+    self.raw_fitness.append(max_raw)
+    self.app_fitness.append(max(ap_fit))
+
+    self.to_tsne_organisms = self.init_organisms.copy()
+
+    self.tsne_fitness += [self.cfs.fitness(i) for i in self.init_organisms]
+
+    while round(max_raw, 2) != 1.00 and generation < 1000: 
+      
+      fittest_organisms, most_fit_organism = self.cfs.selection(self.init_organisms, ap_fit)
+      new_population = self.cfs.crossover_mutation(fittest_organisms)
+      mutated_population = self.cfs.mutation_flip(new_population)
+
+      self.init_organisms = mutated_population.copy()
+
+      self.to_tsne_organisms = np.append(self.to_tsne_organisms, self.init_organisms, axis = 0)
+      self.tsne_fitness += [self.cfs.fitness(i) for i in self.init_organisms]
+
+      self.competent_mod, _ = self.cfs.reorganize(self.init_organisms)
+
+      r_fit = [self.cfs.fitness(i) for i in self.init_organisms]
+      ap_fit = [self.cfs.fitness(j) for j in self.competent_mod]
+
+      max_raw = max(r_fit)
+      max_ap = max(ap_fit)
+
+      self.raw_fitness.append(max_raw)
+      self.app_fitness.append(max_ap)
+
+      generation +=1
+
+      print("GEN: {} | RAW_FITNESS: {:.2f} | APP_FITNESS: {:.2f} | Tsne_size: {}".format(generation, max_raw, max_ap, (self.to_tsne_organisms.shape, len(self.tsne_fitness)))) 
+
+
+  def plot_fitness(self): 
+    plt.rcParams["figure.figsize"] = (20,10)
+    plt.plot(len(self.raw_fitness), self.raw_fitness, label = 'Raw_fitness')
+    plt.plot(len(self.app_fitness), self.app_fitness, label = 'App_fitness')
+    plt.xlabel('Generations')
+    plt.ylabel('Max Fitness')
+    plt.title('Same organism, two fitness levels')
+    plt.show()
+
+  def tsne_plot(self, nos):
+    plt.rcParams["figure.figsize"] = (20,10)
+    print('TSNE in progress for data matrix of size: {}'.format(self.to_tsne_organisms[nos, :].shape))
+    X_embedded = TSNE(n_components=2).fit_transform(self.to_tsne_organisms[nos, :])
+
+    print('TSNE completed\n')
+    print('Embedding Dimension: {}'.format(X_embedded.shape))
+    
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    ax.plot(X_embedded[:,0], X_embedded[:,1], self.tsne_fitness, label='fitness curve')
+    ax.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+  conf = {
+  'LOW' :1,
+  'HIGH':101,
+  'N_organisms' : 100,
+  'SIZE' : 100,
+  'Mutation_Probability' : 0.6,
+  }
+
+  cfs = HelperFuncs(conf)
+  run1 = TwoFit(conf, cfs)
+
+  run1.run_ga()
+  run1.plot_fitness()
+  run1.tsne_plot(100)
